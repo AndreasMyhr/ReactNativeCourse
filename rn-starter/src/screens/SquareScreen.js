72f8777190842bceb3b5588eadefa213b519c4de
andreastm27 <48515090+andreastm27@users.noreply.github.com>
2020-06-24 17:46:13 +0200

    latest work

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,10 +1,29 @@
-import React, { useState }  from 'react';
+import React, { useState, useReducer }  from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const COLOR_INCREMENT=15;
 
+const reducer=(state,action)=>{
+    //state= {red: number, green: number, blue: number}
+    //action ={color to change, red green or blue : amount 15||-15 }
+
+    switch(action.colorToChange){
+        case 'red':
+            return {...state,red:state.red+action.amount};//
+        case 'blue':
+            return {...state,blue:state.blue+action.amount};
+        case 'green':
+            return {...state,blue:state.blue+action.amount};
+        default:
+            return{...state};
+    }
+};
+
 const SquareScreen =() =>{
+
+    const[state,dispatch]=useReducer(reducer,{red:0,green:0,blue:0});
+    /*
     const [red, setRed]=useState(0);
     const [green, setGreen]=useState(0);
     const [blue,setBlue]=useState(0);
@@ -23,8 +42,9 @@ const SquareScreen =() =>{
             default:
                 return;
         }
-    
+  
    };
+   */
 
 
     return( <View>
